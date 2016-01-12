20592367608564f7f8656f4d4cacb1e3846aeaf2
Tarun Maheshwari <ectarun@gmail.com>
2016-01-12 18:43:35 +0530

    serevr

--- lib/server.sh
+++ lib/server.sh
@@ -6,21 +6,15 @@ install_server() {
 
     header "Downloading package"
 
-    cd $BUILD_DIR
-    mkdir -p $VENDOR_DIR
-    cd $VENDOR_DIR
-
-
   local download_url="https://github.com/technohowl/poker-eval/archive/master.tar.gz"
   curl "$download_url" --silent --fail -o /tmp/server.tar.gz || (echo "Unable to download server  does it exist?" && false)
   tar xzf /tmp/server.tar.gz -C /tmp
-  mv -f /tmp/poker-eval-master $dir/poker-eval
 
-  cd $dir/poker-eval
+  cd /tmp/poker-eval-master
   pwd
 
   chmod +x configure
-  ./configure --prefix="$BUILD_DIR/vendor"
+  ./configure --prefix="$BUILD_DIR/vendor/"
   pwd
   make && make install
 
