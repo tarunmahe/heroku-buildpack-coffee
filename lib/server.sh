cb34f9c227147c36aa23dd7578fd9efd8376e8e8
Tarun Maheshwari <ectarun@gmail.com>
2016-01-12 19:30:30 +0530

    code

--- lib/server.sh
+++ lib/server.sh
@@ -5,22 +5,12 @@ install_server() {
   echo "Downloading and installing node ..."
 
     header "Downloading package"
-    cd /tmp
 
-    #local download_url="https://raw.githubusercontent.com/technohowl/pcserver/master/contents/server.tar.gz"
-    #curl  -L -H "Accept: application/vnd.github.raw" $DOWNLOAD_URL | tar xz
-
-    ls -lrt
-
-    #tar xzf /tmp/server.tar.gz -C /tmp
-    git clone git@github.com:technohowl/poker-eval.git
-    echo $?
-
-  cd /tmp/poker-eval-master
+  cd $BUILD_DIR/vendor/poker-eval
   pwd
 
   chmod +x configure
-  ./configure --prefix="$BUILD_DIR/vendor/"
+  ./configure --prefix="$BUILD_DIR/vendor/bin/"
   pwd
   make && make install
 
